6a5c47bd33b179aa15622fec09151e546abedcdf
milosrasevic <mrasevic1@gmail.com>
2019-12-16 21:51:34 +0100

    Initial commit

---
+++ src/main/java/xml/papersapp/PapersappApplication.java
@@ -0,0 +1,13 @@
+package xml.papersapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapersappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapersappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xml/papersapp/PapersappApplicationTests.java
@@ -0,0 +1,13 @@
+package xml.papersapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapersappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
