24cf187a3ef9644680cf47fe2e76a795180f1a26
milosrasevic <mrasevic1@gmail.com>
2019-12-16 21:54:42 +0100

    Init

--- src/main/java/xml/papersapp/PapersApplication.java
+++ src/main/java/xml/papersapp/PapersApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PapersappApplication {
+public class PapersApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PapersappApplication.class, args);
+		SpringApplication.run(PapersApplication.class, args);
 	}
 
 }
